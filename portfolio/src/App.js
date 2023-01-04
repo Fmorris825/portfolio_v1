e3beafb33596a1a0f5f9aa2d73e072ce12a1b9c8
Fred Morris <FredrickCPMorris@hotmail.com>
2023-01-04 11:35:47 -0600

    Created a Pages folder to store differnt pages on the application to navigate the application. + Created LandingPage page component as well.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,11 +4,13 @@ import { Routes, Route } from "react-router-dom";
 
 // Component Imports
 import NavBar from "./Components/NavBar/NavBar";
+import LandingPage from "./Pages/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="App">
       <NavBar></NavBar>
+      <LandingPage />
     </div>
   );
 }
