97259c50a40eddf463a35e3b707658797054b768
Fred Morris <FredrickCPMorris@hotmail.com>
2023-01-05 01:04:20 -0600

    Replaced App Start Up with Source code to fix Dom-Router Issue. + Created Landing Page and Projects Page components.

---
+++ frontend/src/App.js
@@ -0,0 +1,32 @@
+// General Imports
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+
+// Pages Imports
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+
+// Component Imports
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+
+// Util Imports
+import PrivateRoute from "./utils/PrivateRoute";
+import LandingPage from "./pages/LandingPage/LandingPage";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
