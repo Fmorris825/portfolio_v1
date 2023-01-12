698735f1b7a4ad249b12b87fb6e414e5510441a4
Fred Morris <FredrickCPMorris@hotmail.com>
2023-01-12 13:45:37 -0600

    Styling landing page.

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,25 +3,29 @@ import { Link } from "react-router-dom";
 
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { NavbarBrand, NavLink } from "react-bootstrap";
+import { NavbarBrand, NavLink, Col, Row } from "react-bootstrap";
 
 import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <Navbar className="navbar">
-      <NavbarBrand className="brand" href="/">
-        <div className="navTextBrand">Fredrick Morris</div>
-      </NavbarBrand>
-      <Nav className="links">
-        <NavLink href="/projects">
-          <div className="navText">Projects</div>
-        </NavLink>
-        <NavLink href="/contactme">
-          <div className="navText">Contact Me</div>
-        </NavLink>
-      </Nav>
-    </Navbar>
+    <Col>
+      <Row>
+        <Navbar className="navbar">
+          <NavbarBrand className="brand" href="/">
+            <div className="navTextBrand">Fredrick Morris</div>
+          </NavbarBrand>
+          <Nav className="links">
+            <NavLink href="/projects">
+              <div className="navText">Projects</div>
+            </NavLink>
+            <NavLink href="/contactme">
+              <div className="navText">Contact Me</div>
+            </NavLink>
+          </Nav>
+        </Navbar>
+      </Row>
+    </Col>
   );
 };
 
