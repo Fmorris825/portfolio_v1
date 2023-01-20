184325a96f40cf378f243a27713dfa7c80b4f79b
Fred Morris <FredrickCPMorris@hotmail.com>
2023-01-20 11:38:32 -0600

    style testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,26 +14,32 @@ import YoutubeClone from "./pages/YoutubeClone/YoutubeClone";
 import Footer from "./components/Footer/Footer";
 import ContactMe from "./pages/ContactMe/ContactMe";
 import Resources from "./pages/Resources/Resources";
-import { Container } from "react-bootstrap";
+import { Container, Col, Row } from "react-bootstrap";
 
 function App() {
   return (
     <Container>
-      <NavBar />
-      <Container className="d-flex justify-content-center">
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/projects" element={<ProjectsPage />} />
-          <Route
-            path="/Morris%20Delta%20Realty"
-            element={<MorrisDeltaRealty />}
-          />
-          <Route path="/Youtube%20Clone" element={<YoutubeClone />} />
-          <Route path="/ContactMe" element={<ContactMe />} />
-          <Route path="/Resources" element={<Resources />} />
-        </Routes>
-      </Container>
-      <Footer />
+      <Col xs={true}>
+        <Row>
+          <NavBar />
+        </Row>
+        <Row>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route
+              path="/Morris%20Delta%20Realty"
+              element={<MorrisDeltaRealty />}
+            />
+            <Route path="/Youtube%20Clone" element={<YoutubeClone />} />
+            <Route path="/ContactMe" element={<ContactMe />} />
+            <Route path="/Resources" element={<Resources />} />
+          </Routes>
+        </Row>
+        <Row>
+          <Footer />
+        </Row>
+      </Col>
     </Container>
   );
 }
