51c8b2abd5fd13464052f081883ca6cde0ce87fa
Fred Morris <FredrickCPMorris@hotmail.com>
2023-01-21 03:25:08 -0600

    Added conditional statement for apps without live applications.

--- frontend/src/components/Project/Project.jsx
+++ frontend/src/components/Project/Project.jsx
@@ -4,6 +4,32 @@ import ProjectThumbnailCard from "./ProjectThumbnailCard.jsx/ProjectThumbnailCar
 
 const Project = ({ project }) => {
   console.log(project);
+  if (!project.project_url)
+    return (
+      <Row className="d-flex justify-content-center m-3">
+        <Col>
+          <Image className="thumbnails grow" src={project.project_thumbnail} />
+        </Col>
+        <Col>
+          <Container>
+            <Row className="d-flex justify-content-center m-3">
+              {project.project_name}
+            </Row>
+            <Row>
+              <a
+                className="d-flex justify-content-center"
+                href={project.project_github}
+              >
+                Github Repository
+              </a>
+            </Row>
+            <Row className="contain m-3">
+              <p>{project.project_description}</p>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+    );
   return (
     <Row className="d-flex justify-content-center m-3">
       <Col>
